64ca93001236b2a94f7a99be064b9e05da68cf89
Yahya afadisse <yahyaafadisse92@gmail.com>
2025-10-22 23:35:39 +0100

    fixed problem in configuration problem in cashing

--- src/main/java/org/example/controller/TestController.java
+++ src/main/java/org/example/controller/TestController.java
@@ -4,14 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class TestController {
 
     @GetMapping("/hello")
-    public HashMap<String,Object> sayHello() {
-        HashMap<String,Object> obj = new HashMap<>();
-        obj.put("Message","Hello world");
-        return obj;
+    public Map<String, Object> sayHello() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("message", "hello world");
+        response.put("version", "1.0");
+        return response;
     }
 }
